bf2ba2c10c1ddfed307ff37671522c2213686fb2
Maulanya <maulanarizq26@gmail.com>
2024-10-10 15:03:33 +0800

    feat: add login with google

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const { createClient } = require("@supabase/supabase-js");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+const supabaseUrl = process.env.URL;
+const supabaseKey = process.env.KEY;
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
@@ -18,6 +27,83 @@ app.use(function (req, res, next) {
 });
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.options("*", cors());
+
+app.post("/decode-token", async (req, res) => {
+  const accessToken = req.body.access_token;
+
+  if (!accessToken) {
+    return res.status(400).send("No access token provided.");
+  }
+
+  try {
+    const decoded = jwt.decode(accessToken);
+    const res = await fetch("https://api.growtavern.site/player/login/google", {
+      method: "post",
+      header: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: decoded.email,
+      }),
+    });
+    const requestdata = await res.json();
+    if (requestdata.type === "success") {
+      fetch("/player/growid/login/validate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          growId: requestdata.data.name,
+          password: requestdata.data.pass,
+        }),
+      });
+    }
+    return res.sendStatus(200);
+  } catch (error) {
+    return res.status(500).send("Error decoding token.");
+  }
+});
+
+app.post("/player/auth/google", async (req, res) => {
+  try {
+    const { data, error } = await supabaseClient.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo:
+          "https://growtopia-login.vercel.app/public/html/dashboard.html",
+      },
+    });
+
+    if (error) {
+      return res.status(400).json({
+        status: "error",
+        message: error.message,
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      url: data.url,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong during the Google authentication process.",
+    });
+  }
+});
 
 app.post("/player/login/dashboard", (req, res) => {
   res.sendFile(__dirname + "/public/html/dashboard.html");
