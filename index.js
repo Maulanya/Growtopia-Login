1d2f3488c17c4395a479f32898a38bf11d80cd2f
Maulanya <maulanarizq26@gmail.com>
2024-10-11 16:46:32 +0800

    push

--- index.js
+++ index.js
@@ -164,8 +164,8 @@ app.post("/player/growid/login/validate", (req, res) => {
 
 app.get("/", (req, res) => {
   // res.send("Hello World");
-  // res.sendFile(__dirname + "/public/html/dashboard.html");
-  res.send("<script>window.close();</script>");
+  res.sendFile(__dirname + "/public/html/dashboard.html");
+  // res.send("<script>window.close();</script>");
 });
 
 app.use(function (req, res) {
