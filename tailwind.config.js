d4c636b8d874a3f5a5a241b082fa3c63a9bf6d1d
Maulanya <maulanarizq26@gmail.com>
2024-10-22 11:47:15 +0800

    add audio and redesign dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: ["./public/html/**/*.html"],
   theme: {
     extend: {
-      fontFamily: {
-        GeorticBold: "Geortic-Bold, sans-serif",
-      },
       screens: {
         portrait: {
           raw: "(orientation: portrait)",
