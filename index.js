a9570ab369bf538ce8e57399b40e43edb1076d43
Maulanya <maulanarizq26@gmail.com>
2024-12-03 09:50:17 +0800

    fix: add animation and fix some login route

--- index.js
+++ index.js
@@ -220,9 +220,31 @@ app.post("/player/login/dashboard", (req, res) => {
 app.post("/player/growid/login/validate", (req, res) => {
   const growId = req.body.growId;
   const password = req.body.password;
+  const { refreshToken, clientData } = req.body;
 
+  // const token = Buffer.from(
+  //   `_token=&growId=${growId}&password=${password}`
+  // ).toString("base64");
+
+  if (!refreshToken || !clientData) {
+    return res.status(400).send({
+      status: "error",
+      message: "Missing refreshToken or clientData",
+    });
+  }
+
+  let decodeRefreshToken = Buffer.from(refreshToken, "base64").toString(
+    "utf-8"
+  );
+  if (!decodeRefreshToken.includes("&from=")) {
+    decodeRefreshToken += "&from=session";
+  }
+  console.log(decodeRefreshToken);
   const token = Buffer.from(
-    `_token=&growId=${growId}&password=${password}`
+    decodeRefreshToken.replace(
+      /(_token=)[^&]*/,
+      `$1${Buffer.from(clientData).toString("base64")}`
+    )
   ).toString("base64");
 
   res.send(
